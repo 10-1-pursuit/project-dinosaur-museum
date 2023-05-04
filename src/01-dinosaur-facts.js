f68fe29c624355ed7566fbc31243999d4b27402e
Jesse Chong <jessechong@pursuit.org>
2023-05-04 17:24:59 -0400

    Need to write comments for third question

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,34 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+function getLongestDinosaur(dinosaurs) {
+  let bigDinosaur = null; //null is a placeholder.
+
+  for(const dinosaur of dinosaurs) { // iterates over the array dinosaurs.
+    if (!bigDinosaur || dinosaur.lengthInMeters > bigDinosaur.lengthInMeters) { // if !bigDinosaur(*is null*) OR dinosaurs that are being iterated are bigger than bigDinosaur
+      bigDinosaur = dinosaur; // if either of these happen then bigDinosaur is updated to that dinosaur that is being iterated.
+      } 
+    }
+
+  if (bigDinosaur === null){ // the strictly equals make sure if bigDinosaur only equals null then return an empty object, as said in test
+    return {};
+  }
+
+  const bigDinosaurInFeet = {
+    [bigDinosaur.name]: bigDinosaur.lengthInMeters * 3.281 // bigDinosaur.name(new object that is DYNAMIC and has a SINGPLE KEY-VALUE PAIR)the KEY is set to the name of the biggest dinosaur
+  }; // which was obtained by bigDinosaur and the biggest dinosaur is multiplied to match feet.
+
+  return bigDinosaurInFeet
+}
+
+// function getLongestDinosaur(dinosaurs) {
+//   let bigDinosaur = null;
+
+//   for(const dinosaur of dinosaurs) { // Using conditional ternary operator ?(if statement) truthy:falsy,
+//     bigDinosaur = !bigDinosaur || dinosaur.lengthInMeters > bigDinosaur.lengthInMeters ? dinosaur : bigDinosaur //if !bigDinosaur(*is null*) OR dinosaurs 
+//   } // that are being iterated are bigger than bigDinosaur then it assigns current biggest dinosaur to bigDinosaur, if not then keep it the same.
+//   return bigDinosaur ? {[bigDinosaur.name]: bigDinosaur.lengthInMeters * 3.281} : {}; // using conditional ternary operator bigDinosaur.name(new object that is DYNAMIC and has a 
+// } // SINGLE KEY-VALUE PAIR) (KEY is set to the name of the biggest dinosaur)  return bigDinosaur if the biggest dinosaur and multiply lengthInMeters to feet, or return an empty object
 
 /**
  * getDinosaurDescription()
@@ -44,14 +71,24 @@ function getLongestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  for (let dinosaur of dinosaurs) {
+    if (id === dinosaur.dinosaurId) { // if the STRING (id) equals dinosaur id
+      years = Math.min(...dinosaur.mya) // years EQUALS Math.min(find the smallest value in array) using the spread operator
+      return `${dinosaur.name} (${dinosaur.pronunciation})\n${dinosaur.info} It lived in the ${dinosaur.period} period, over ${years} million years ago.`
+    }//Math.min takes two arguments if available
+  }
+  return "A dinosaur with an ID of 'incorrect-id' cannot be found."
+}
 
 /**
  * getDinosaursAliveMya()
  * ---------------------
- * Returns an array of dinosaurs who were alive at the given `mya` (i.e. "millions of years ago") value. If a `key` is provided, returns the value of that key for each dinosaur alive at that time. Otherwise, returns the ID.
+ * Returns an array of dinosaurs who were alive at the given `mya` (i.e. "millions of years ago") value. 
+ * If a `key` is provided, returns the value of that key for each dinosaur alive at that time. Otherwise, returns the ID.
  *
- * If the dinosaur only has a single value for `mya`, allows for the `mya` value to be equal to the given value or one less. For example, if a dinosaur has a `mya` value of `[29]`, the dinosaur's information will be returned if `29` is entered or `28` is entered.
+ * If the dinosaur only has a single value for `mya`, allows for the `mya` value to be equal to the given value or one less. 
+ * For example, if a dinosaur has a `mya` value of `[29]`, the dinosaur's information will be returned if `29` is entered or `28` is entered.
  *
  * @param {Object[]} dinosaurs - An array of dinosaur objects. See the `data/dinosaurs.js` file for an example of the input.
  * @param {number} mya - "Millions of years ago."
@@ -71,7 +108,31 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) {
+  let addArr = key;
+  const result = [];
+
+  for (let dinosaur of dinosaurs) {
+    if (dinosaur[addArr] === undefined) {
+      addArr = "dinosaurId"
+    }
+
+    if (dinosaur.mya.length === 1 && (mya === dinosaur.mya[0] || mya === (dinosaur.mya[0] -1))) {
+      result.push(dinosaur[addArr])
+    }
+    else if (mya <= dinosaur.mya[0] && mya >= dinosaur.mya[1]) {
+      result.push(dinosaur[addArr])
+    }
+  }
+  return result
+}
+
+// ✕ should return the IDs of all dinosaurs that were alive approximately at the given time (1 ms)
+// ✕ should include dinosaurs with only one `mya` year
+// ✕ if the `mya` key is an array of one number, should allow for 1 MYA less than the amount (1 ms)
+// ✕ should return an empty array if the year does not match any
+// ✕ if the third argument is set, should replace the IDs with the value of the given key
+// ✕ if the third argument is set, but to a key that doesn't return a value, should return the ID
 
 module.exports = {
   getLongestDinosaur,
