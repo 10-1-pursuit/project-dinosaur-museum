f8fc703b2f2d53b011a0f8210a2a6ce36949f1a4
Yanako Lajara <yanakolajara@pursuit.org>
2023-05-06 16:18:24 -0400

    getAllFactsTogether function has been made, as well as other minor bugs has been fixed

--- src/04-dinosaur-extra-facts.js
+++ src/04-dinosaur-extra-facts.js
@@ -32,39 +32,54 @@ function getThreeLongestDinosaurs(dinosaursArrayList){
     let secondObj = dinosaursArrayList.find(dinosaur => dinosaur.dinosaurId === secondId)
     let thirdObj = dinosaursArrayList.find(dinosaur => dinosaur.dinosaurId === thirdId)
     
-    return `${formatLine()}\nTop 3 longest dinosaurs:\n\n1. ${firstObj.name} (${firstObj.lengthInMeters} meters)\n2. ${secondObj.name} (${secondObj.lengthInMeters} meters)\n3. ${thirdObj.name} (${thirdObj.lengthInMeters} meters)\n${formatLine()}\n`;
+    return `${formatLine()}\nTop 3 longest dinosaur on earth:\n1. ${firstObj.name} (${firstObj.lengthInMeters} meters)\n2. ${secondObj.name} (${secondObj.lengthInMeters} meters)\n3. ${thirdObj.name} (${thirdObj.lengthInMeters} meters)\n${formatLine()}\n`;
 }
 
 function getThreeOldestDinosaurs(dinosaursArrayList){
-    return;
+    let first = .0003
+    let firstId = ""
+    let second = .0002
+    let secondId = ""
+    let third = .0001
+    let thirdId = ""
+    dinosaursArrayList.forEach(dinosaur => {
+        if(dinosaur.mya[0] > third &&
+            dinosaur.mya[0] > second &&
+            dinosaur.mya[0] > first){
+                first = dinosaur.mya[0]
+                firstId = dinosaur.dinosaurId
+        }else if(dinosaur.mya[0] > third &&
+            dinosaur.mya[0] > second &&
+            dinosaur.mya[0] < first){
+                second = dinosaur.mya[0]
+                secondId = dinosaur.dinosaurId
+        }else if(dinosaur.mya[0] > third &&
+            dinosaur.mya[0] < second &&
+            dinosaur.mya[0] < first){
+                third = dinosaur.mya[0]
+                thirdId = dinosaur.dinosaurId
+        }
+    })
+    let firstObj = dinosaursArrayList.find(dinosaur => dinosaur.dinosaurId === firstId)
+    let secondObj = dinosaursArrayList.find(dinosaur => dinosaur.dinosaurId === secondId)
+    let thirdObj = dinosaursArrayList.find(dinosaur => dinosaur.dinosaurId === thirdId)
+    return `${formatLine()}\nTop 3 oldest dinosaurs on earth:\n1. ${firstObj.name} (${firstObj.mya[0]} million years ago)\n2. ${secondObj.name} (${secondObj.mya[0]} million years ago)\n3. ${thirdObj.name} (${thirdObj.mya[0]} million years ago)\n${formatLine()}\n`;
 }
 
 function getAllSamePeriodDinosaurs(dinosaursArrayList, period){
-    return;
+    let dinosaurArr = dinosaursArrayList.filter(dinosaur => dinosaur.period === period)
+    let names = (dinosaurArr.map(dinosaur => dinosaur.name)).join(', ')
+    return `${formatLine()}\nDinosaurs at ${period}:\n${names}\n${formatLine()}\n`;
 }
 
 function getAllSameDietDinosaurs(dinosaursArrayList, diet){
-    return;
+    let dinosaurArr = dinosaursArrayList.filter(dinosaur => dinosaur.diet === diet)
+    let names = (dinosaurArr.map(dinosaur => dinosaur.name)).join(', ')
+    return `${formatLine()}\n${diet} dinosaurs:\n${names}\n${formatLine()}\n`;
 }
 
-function getAllFactsTogether(dinosaursArrayList){
-    
-    // TODO: add all functions with the next format
-    /**
-     * (The ${} must be filled with the arrays from the result of the functions)
-     * 
-     * Top 3 longest animlas:
-     * FIXME: let arrayFromFunction = function()
-     * 1. ${DinosaurFromArray}
-     * 2. ${DinosaurFromArray}
-     * 3. ${DinosaurFromArray}
-     * 
-     * Top 3 oldest animals:
-     * ...
-     * 
-    */
-    return `${getThreeLongestDinosaurs(dinosaursArrayList)}\n
-    `;
+function getAllFactsTogether(dinosaursArrayList, period, diet){
+return `${getThreeLongestDinosaurs(dinosaursArrayList)}\n${getThreeOldestDinosaurs(dinosaursArrayList)}\n${getAllSamePeriodDinosaurs(dinosaurs, period)}\n${getAllSameDietDinosaurs(dinosaurs, diet)}`;
 }
 
-console.log(getAllFactsTogether(dinosaurs))
+console.log(getAllFactsTogether(dinosaurs, "Late Jurassic", "carnivorous"))
