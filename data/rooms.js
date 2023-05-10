f37f15add2759943bf47e6978a30d475a24bc110
anickacodes <anickalewis@pursuit.org>
2023-05-10 11:22:05 -0400

    I accidentally made changes here, apologies

--- data/rooms.js
+++ data/rooms.js
@@ -74,7 +74,7 @@ const rooms = [
     ],
   },
   {
-    roomId: "Ys2Trg-1OT", // 
+    roomId: "Ys2Trg-1OT", // 7
     name: "Terrell Leon Lecture Room",
     requiredTicketPermissions: ["education"],
     dinosaurs: [],
